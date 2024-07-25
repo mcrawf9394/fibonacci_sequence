5b918fd4404fefe52f5ea129f79e05e3ef64fafe
mcrawf9394 <mjc9394@gmail.com>
2024-07-25 11:15:34 -0400

    Initial commit

---
+++ target/debug/deps/first_project-28424dd76e12e825.d
@@ -0,0 +1,5 @@
+/home/sam/personalRepos/Rust-Projects/first-project/target/debug/deps/libfirst_project-28424dd76e12e825.rmeta: src/main.rs
+
+/home/sam/personalRepos/Rust-Projects/first-project/target/debug/deps/first_project-28424dd76e12e825.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/first_project-e1d936bdfebc02e4.d
@@ -0,0 +1,5 @@
+/home/sam/personalRepos/Rust-Projects/first-project/target/debug/deps/libfirst_project-e1d936bdfebc02e4.rmeta: src/main.rs
+
+/home/sam/personalRepos/Rust-Projects/first-project/target/debug/deps/first_project-e1d936bdfebc02e4.d: src/main.rs
+
+src/main.rs:
